1451afc3d4cee5ec1ca8c9970886b8849ed67b63
Allan Baguet <allan.baguet80@gmail.com>
2023-09-11 13:55:18 +0200

    à finir avec le modele MVC

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+//fait appel au controllers, et donc redirige vers cette dernière
+//toute la logique du PHP se trouve dans userSignUp-ctrl.php
+    header('location: controllers/userSignUp-ctrl.php');
+    die;
